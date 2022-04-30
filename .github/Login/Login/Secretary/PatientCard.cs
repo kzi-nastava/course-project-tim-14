0fc624a4cb4a39527b93c40bb7b03fc143fb1ce4
Milenko Krasavcic <krasavcicmilenko@gmail.com>
2022-04-30 12:05:58 +0200

    Patient card class
    Created PatientCard class

---
+++ .github/Login/Login/Secretary/PatientCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.Secretary
+{
+    class PatientCard
+    {
+        public int CardId;
+        public Patient Patient;
+        public int Height;
+        public int Weight;
+        public string[] PreviouseIllnesses;
+        public string[] DoctorNotes;
+    }
+
+    public PatientCard(int cardId, PatientCard patient, int height, int weight, string[] previousIllnesses, string[] doctorNotes)
+    {
+        CardId = cardId;
+        Patient = patient;
+        Height = height;
+        Weight = weight;
+        PreviouseIllnesses = previousIllnesses;
+        DoctorNotes = doctorNotes;
+    }
+}
