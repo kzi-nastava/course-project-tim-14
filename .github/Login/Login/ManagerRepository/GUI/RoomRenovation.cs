8a5a705507dd9a650b96df4c4531ce5ce827de19
sv75-2020 <suput.sv75.2020@uns.ac.rs>
2022-05-14 22:17:47 +0200

    Room Renovation
    Room Renovation class

---
+++ .github/Login/Login/ManagerRepository/GUI/RoomRenovation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login.ManagerRepository.GUI
+{
+    public partial class RoomRenovation : Form
+    {
+        private CheckupRepository checkupRepository;
+        private RoomsRepository roomsRepository;
+
+        public RoomRenovation(CheckupRepository checkupRepository, RoomsRepository roomsRepository)
+        {
+            InitializeComponent();
+            this.checkupRepository = checkupRepository;
+            this.roomsRepository = roomsRepository;
+        }
+
+        private void confirmRenovation_Click(object sender, EventArgs e)
+        {
+            DateTime renovationStart = startDate.Value.Date;
+            DateTime renovationEnd = endDate.Value.Date;
+            int validDatePicked = DateTime.Compare(renovationStart, renovationEnd);
+            bool roomAvailable = true;
+            Room room = roomsRepository.FindRoom(Int32.Parse(roomIdBox.Text));
+            if (validDatePicked < 0)
+            {
+                if (room.type == "ExaminatioRoom") {
+                    foreach (Checkup checkup in checkupRepository.checkups)
+                    {
+                        if (checkup.dateTime > renovationStart && checkup.dateTime < renovationEnd)
+                        {
+                            roomAvailable = false;
+                            break;
+                        }
+                    }
+                }
+                if (roomAvailable) {
+                    Renovation renovation = new Renovation(Int32.Parse(roomIdBox.Text), renovationStart, renovationEnd);
+                    roomsRepository.WriteToFile(renovation.ToFile());
+                    MessageBox.Show("Renovation confirmed.", "Success");
+                }
+                else
+                {
+                    MessageBox.Show("The room isn't available for the selected dates.", "Error");
+                }
+            } else
+            {
+                MessageBox.Show("Start date must be earlier than end date.", "Error");
+            }
+
+        }
+
+        private void RoomRenovation_Load(object sender, EventArgs e)
+        {
+            startDate.MinDate = DateTime.Today;
+            endDate.MinDate = DateTime.Today;
+        }
+    }
+}
