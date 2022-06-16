eaffb702518b74a5a3d90708865c52a754a677f3
sv75-2020 <suput.sv75.2020@uns.ac.rs>
2022-06-16 22:03:37 +0200

    Added doctor results

---
+++ Login/Surveys/DoctorSurveyResultsForm.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace Login.Surveys
+{
+    partial class DoctorSurveyResultsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.commentsListBox = new System.Windows.Forms.ListBox();
+            this.moreInfoRecommend = new System.Windows.Forms.Button();
+            this.moreInfoSatisfaction = new System.Windows.Forms.Button();
+            this.moreInfoQuality = new System.Windows.Forms.Button();
+            this.recommendAvg = new System.Windows.Forms.TextBox();
+            this.satisfactionAvg = new System.Windows.Forms.TextBox();
+            this.qualityAvg = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.doctorComboBox = new System.Windows.Forms.ComboBox();
+            this.confirmBtn = new System.Windows.Forms.Button();
+            this.worstDoctorsBtn = new System.Windows.Forms.Button();
+            this.bestDoctorsBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // commentsListBox
+            // 
+            this.commentsListBox.FormattingEnabled = true;
+            this.commentsListBox.ItemHeight = 20;
+            this.commentsListBox.Location = new System.Drawing.Point(554, 208);
+            this.commentsListBox.Name = "commentsListBox";
+            this.commentsListBox.Size = new System.Drawing.Size(213, 184);
+            this.commentsListBox.TabIndex = 41;
+            // 
+            // moreInfoRecommend
+            // 
+            this.moreInfoRecommend.Location = new System.Drawing.Point(397, 348);
+            this.moreInfoRecommend.Name = "moreInfoRecommend";
+            this.moreInfoRecommend.Size = new System.Drawing.Size(90, 43);
+            this.moreInfoRecommend.TabIndex = 40;
+            this.moreInfoRecommend.Text = "more info";
+            this.moreInfoRecommend.UseVisualStyleBackColor = true;
+            // 
+            // moreInfoSatisfaction
+            // 
+            this.moreInfoSatisfaction.Location = new System.Drawing.Point(397, 277);
+            this.moreInfoSatisfaction.Name = "moreInfoSatisfaction";
+            this.moreInfoSatisfaction.Size = new System.Drawing.Size(90, 34);
+            this.moreInfoSatisfaction.TabIndex = 39;
+            this.moreInfoSatisfaction.Text = "more info";
+            this.moreInfoSatisfaction.UseVisualStyleBackColor = true;
+            // 
+            // moreInfoQuality
+            // 
+            this.moreInfoQuality.Location = new System.Drawing.Point(397, 203);
+            this.moreInfoQuality.Name = "moreInfoQuality";
+            this.moreInfoQuality.Size = new System.Drawing.Size(90, 34);
+            this.moreInfoQuality.TabIndex = 37;
+            this.moreInfoQuality.Text = "more info";
+            this.moreInfoQuality.UseVisualStyleBackColor = true;
+            // 
+            // recommendAvg
+            // 
+            this.recommendAvg.Location = new System.Drawing.Point(280, 356);
+            this.recommendAvg.Name = "recommendAvg";
+            this.recommendAvg.Size = new System.Drawing.Size(71, 26);
+            this.recommendAvg.TabIndex = 36;
+            // 
+            // satisfactionAvg
+            // 
+            this.satisfactionAvg.Location = new System.Drawing.Point(280, 281);
+            this.satisfactionAvg.Name = "satisfactionAvg";
+            this.satisfactionAvg.Size = new System.Drawing.Size(71, 26);
+            this.satisfactionAvg.TabIndex = 35;
+            // 
+            // qualityAvg
+            // 
+            this.qualityAvg.Location = new System.Drawing.Point(280, 207);
+            this.qualityAvg.Name = "qualityAvg";
+            this.qualityAvg.Size = new System.Drawing.Size(71, 26);
+            this.qualityAvg.TabIndex = 33;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(33, 148);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(281, 20);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "The average score for choosen doctor:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 20);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Quality of service";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 20);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Patients\' satisfaction";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 348);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 20);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Would patients\' recommend";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 368);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 20);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "this doctor to someone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(607, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Comments";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 25;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(44, 33);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(130, 20);
+            this.label9.TabIndex = 42;
+            this.label9.Text = "Choose a doctor:";
+            // 
+            // doctorComboBox
+            // 
+            this.doctorComboBox.FormattingEnabled = true;
+            this.doctorComboBox.Location = new System.Drawing.Point(39, 64);
+            this.doctorComboBox.Name = "doctorComboBox";
+            this.doctorComboBox.Size = new System.Drawing.Size(158, 28);
+            this.doctorComboBox.TabIndex = 45;
+            // 
+            // confirmBtn
+            // 
+            this.confirmBtn.Location = new System.Drawing.Point(261, 58);
+            this.confirmBtn.Name = "confirmBtn";
+            this.confirmBtn.Size = new System.Drawing.Size(90, 38);
+            this.confirmBtn.TabIndex = 46;
+            this.confirmBtn.Text = "Confirm";
+            this.confirmBtn.UseVisualStyleBackColor = true;
+            this.confirmBtn.Click += new System.EventHandler(this.confirmBtn_Click);
+            // 
+            // worstDoctorsBtn
+            // 
+            this.worstDoctorsBtn.Location = new System.Drawing.Point(624, 33);
+            this.worstDoctorsBtn.Name = "worstDoctorsBtn";
+            this.worstDoctorsBtn.Size = new System.Drawing.Size(143, 63);
+            this.worstDoctorsBtn.TabIndex = 47;
+            this.worstDoctorsBtn.Text = "Lowest Rated Doctors";
+            this.worstDoctorsBtn.UseVisualStyleBackColor = true;
+            this.worstDoctorsBtn.Click += new System.EventHandler(this.worstDoctorsBtn_Click);
+            // 
+            // bestDoctorsBtn
+            // 
+            this.bestDoctorsBtn.Location = new System.Drawing.Point(477, 33);
+            this.bestDoctorsBtn.Name = "bestDoctorsBtn";
+            this.bestDoctorsBtn.Size = new System.Drawing.Size(130, 63);
+            this.bestDoctorsBtn.TabIndex = 48;
+            this.bestDoctorsBtn.Text = "Highest Rated Doctors";
+            this.bestDoctorsBtn.UseVisualStyleBackColor = true;
+            this.bestDoctorsBtn.Click += new System.EventHandler(this.bestDoctorsBtn_Click);
+            // 
+            // DoctorSurveyResultsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bestDoctorsBtn);
+            this.Controls.Add(this.worstDoctorsBtn);
+            this.Controls.Add(this.confirmBtn);
+            this.Controls.Add(this.doctorComboBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.commentsListBox);
+            this.Controls.Add(this.moreInfoRecommend);
+            this.Controls.Add(this.moreInfoSatisfaction);
+            this.Controls.Add(this.moreInfoQuality);
+            this.Controls.Add(this.recommendAvg);
+            this.Controls.Add(this.satisfactionAvg);
+            this.Controls.Add(this.qualityAvg);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DoctorSurveyResultsForm";
+            this.Text = "DoctorSurveyResultsForm";
+            this.Load += new System.EventHandler(this.DoctorSurveyResultsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox commentsListBox;
+        private System.Windows.Forms.Button moreInfoRecommend;
+        private System.Windows.Forms.Button moreInfoSatisfaction;
+        private System.Windows.Forms.Button moreInfoQuality;
+        private System.Windows.Forms.TextBox recommendAvg;
+        private System.Windows.Forms.TextBox satisfactionAvg;
+        private System.Windows.Forms.TextBox qualityAvg;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox doctorComboBox;
+        private System.Windows.Forms.Button confirmBtn;
+        private System.Windows.Forms.Button worstDoctorsBtn;
+        private System.Windows.Forms.Button bestDoctorsBtn;
+    }
+}
---
+++ Login/Surveys/DoctorSurveyResultsForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login.Surveys
+{
+    public partial class DoctorSurveyResultsForm : Form
+    {
+        public readonly SurveyRepository surveyRepository;
+        public List<Doctor> doctors;
+        public ScoreCalculator scoreCalculator;
+        public DoctorSurveyResultsForm(SurveyRepository surveyRepository, List<Doctor> doctors)
+        {
+            InitializeComponent();
+            this.surveyRepository = surveyRepository;
+            this.doctors = doctors;
+        }
+
+        private void confirmBtn_Click(object sender, EventArgs e)
+        {
+            List<Survey> surveysForDoctor = new List<Survey>();
+            string doctorName = doctorComboBox.SelectedItem.ToString();
+            foreach (Survey survey in surveyRepository.surveys)
+            {
+                if (survey.type == doctorName)
+                {
+                    surveysForDoctor.Add(survey);
+                }
+            }
+            scoreCalculator = new ScoreCalculator(surveysForDoctor);
+            FillTextBoxes();
+            FillComments(surveysForDoctor);
+        }
+
+        private void DoctorSurveyResultsForm_Load(object sender, EventArgs e)
+        {
+            foreach(Doctor doctor in doctors)
+            {
+                doctorComboBox.Items.Add(doctor.name);
+            }
+        }
+
+        private void FillTextBoxes()
+        {
+            qualityAvg.Text = scoreCalculator.quality.ToString();
+            qualityAvg.ReadOnly = true;
+            recommendAvg.Text = scoreCalculator.recommend.ToString();
+            recommendAvg.ReadOnly = true;
+            satisfactionAvg.Text = scoreCalculator.rating.ToString();
+            satisfactionAvg.ReadOnly = true;
+        }
+
+        private void FillComments(List<Survey> surveysForDoctor)
+        {
+            foreach (Survey survey in surveysForDoctor)
+            {
+                commentsListBox.Items.Add(survey.comment);
+            }
+        }
+
+        private void bestDoctorsBtn_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, double> doctorAvgScore = CalculateAvgScores();
+
+            var kvpGroups = doctorAvgScore.OrderByDescending(x => x.Value).GroupBy(x => x.Value).Take(3);
+            string threeBestDoctors = "";
+            foreach (var kvpGroup in kvpGroups)
+            {
+                foreach (var kvp in kvpGroup)
+                {
+                    threeBestDoctors += kvp.Key + " " + kvp.Value.ToString() + "\n";
+                }
+            }
+            MessageBox.Show(threeBestDoctors, "Highest Rated Doctors");
+
+        }
+
+        private void worstDoctorsBtn_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, double> doctorAvgScore = CalculateAvgScores();
+
+            var kvpGroups = doctorAvgScore.OrderBy(x => x.Value).GroupBy(x => x.Value).Take(3);
+            string threeWorstDoctors = "";
+            foreach (var kvpGroup in kvpGroups)
+            {
+                foreach (var kvp in kvpGroup)
+                {
+                    threeWorstDoctors += kvp.Key + " " + kvp.Value.ToString() + "\n";
+                }
+            }
+            MessageBox.Show(threeWorstDoctors, "Lowest Rated Doctors");
+        }
+
+        private Dictionary<string, double> CalculateAvgScores()
+        {
+            Dictionary<string, double> doctorAvgScore = new Dictionary<string, double>();
+            foreach (Doctor doctor in doctors)
+            {
+                List<Survey> surveysForDoctor = new List<Survey>();
+                string doctorName = doctor.name;
+                foreach (Survey survey in surveyRepository.surveys)
+                {
+                    if (survey.type == doctorName)
+                    {
+                        surveysForDoctor.Add(survey);
+                    }
+                }
+                scoreCalculator = new ScoreCalculator(surveysForDoctor);
+                double totalScore = scoreCalculator.CalculateTotal();
+                doctorAvgScore.Add(doctorName, totalScore);
+            }
+            return doctorAvgScore;
+        }
+
+    }
+}
